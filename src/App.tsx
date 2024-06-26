766a8c736a0a485270fabe96786d2ba536eac029
DarkFlarex <darkflarexxx@gmail.com>
2024-06-27 03:30:33 +0600

    Создал проект с компонентом Toolbar для header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Toolbar from "./components/Toolbar/Toolbar";
+
+const App =() => {
+
+
+  return (
+      <>
+        <header>
+          <Toolbar/>
+        </header>
+      </>
+  )
+}
+
+export default App
